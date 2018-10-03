a81986a57c44de191b0cc0a7ed9f3382dd84424e
Benjamin Canac <canacb1@gmail.com>
2018-10-03 19:49:34 +0200

    Try with @provider to instantiate controller

--- providers/ApiDocProvider.js
+++ providers/ApiDocProvider.js
@@ -3,15 +3,18 @@
 const { ServiceProvider } = require('@adonisjs/fold')
 
 class ApiDocProvider extends ServiceProvider {
+  register () {
+    this.app.bind('Cmty/Controllers/ApiDocController', function () {
+      const ApiDocController = require('../src/Controllers/ApiDocController')
+      return new ApiDocController()
+    })
+  }
+
   boot () {
     if (process.env.NODE_ENV !== 'development') return
 
     const Route = use('Adonis/Src/Route')
-    const ApiDocController = require('../src/Controllers/ApiDocController')
-
-    const handler = new ApiDocController()
-
-    Route.get('/api/doc', handler.render)
+    Route.get('/api/doc', '@provider:Cmty/Controllers/ApiDocController.render')
   }
 }
 
